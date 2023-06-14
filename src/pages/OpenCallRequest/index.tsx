e778cfb5632181333b739dbbfcdd028e7d40eadf
Larissa Ferreira <larissa.ferreira@ferreiracosta.com.br>
2023-06-13 22:39:23 -0300

    Adicionando Loading

--- src/pages/OpenCallRequest/index.tsx
+++ src/pages/OpenCallRequest/index.tsx
@@ -13,6 +13,7 @@ import { Fieldset } from "../../components/Fildset";
 import Picker from "react-native-picker-select";
 import React, { ChangeEvent, useState } from "react";
 import { api } from "../../services";
+import Spinner from "../../components/loadingScreen";
 
 export const OpenCallRequest = () => {
   const [tipo, setTipo] = useState("");
@@ -26,18 +27,19 @@ export const OpenCallRequest = () => {
     const numericValue = e.target.value.replace(/[^/0-9]/g, "");
     setSelectedDate(numericValue);
   };
-  
-  const chamadoData = {
-    nome: resumo,
-    tipo: tipo,
-    dataRelato: selectedDate,
-    descricao: descricao,
-    empregado_Matricula: 77777,
-  };
 
   const navigation = useNavigation();
   const CreateTicket = () => {
     setIsLoading(true);
+
+     const chamadoData = {
+       nome: resumo,
+       tipo: tipo,
+       dataRelato: selectedDate,
+       descricao: descricao,
+       empregado_Matricula: 77777,
+     };
+
     api
       .post("/CadastroChamado/", JSON.stringify(chamadoData), {
         headers: {
@@ -58,80 +60,88 @@ export const OpenCallRequest = () => {
 
   return (
     <OpenCallContainer>
-      <BackButton
-        buttonText="voltar"
-        buttonClick={() => useNavigation("Chamado")}
-      />
-      <OpenCallTitle>O que aconteceu?</OpenCallTitle>
-      <OpenCallContent>
-        <Fieldset legend="Resumo">
-          <OpenCallInput
-            value={resumo}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setResumo(e.target.value)
-            }
-            placeholder="Do que se trata o chamado?"
+      {!loading ? (
+        <>
+          <BackButton
+            buttonText="voltar"
+            buttonClick={() => navigation.navigate("Chamado")}
           />
-        </Fieldset>
-        <Fieldset legend="Tipo">
-          <Picker
-            value={tipo}
-            onValueChange={(value) => setTipo(value)}
-            items={[
-              {
-                label: "Falta de material",
-                value: "Falta de material",
-              },
-              {
-                label: "Problema com a internet",
-                value: "Problema com a internet",
-              },
-              {
-                label: "Solicitação de limpeza",
-                value: "Solicitação de limpeza",
-              },
-              {
-                label: "Solicitação de recurso",
-                value: "Solicitação de recurso",
-              },
-              {
-                label: "Objeto perdido",
-                value: "Objeto perdido",
-              },
-              {
-                label: "Outros",
-                value: "Outros",
-              },
-            ]}
-            style={OpenCallPicker}
-          />
-        </Fieldset>
-        <Fieldset legend="Descrição">
-          <OpenCallInput
-            height="240px"
-            placeholder="Nos conte mais detalhes sobre o ocorrido..."
-            value={descricao}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setDescricao(e.target.value)
-            }
-          />
-        </Fieldset>
-        <Fieldset legend="Data do ocorrido">
-          <OpenCallInput
-            type="date"
-            value={selectedDate}
-            onChange={handleInputChange}
-            placeholder="dd/mm/aaaa"
-            keyboardType="numeric"
-            mask="dd/mm/aaaa"
-            maxLength="10"
-            pattern="23/12/2022"
-          />
-        </Fieldset>
-      </OpenCallContent>
-      <FotterButtons>
-        <ConfirmButton>Confirmar</ConfirmButton>
-      </FotterButtons>
+          <OpenCallTitle>O que aconteceu?</OpenCallTitle>
+          <OpenCallContent>
+            <Fieldset legend="Resumo">
+              <OpenCallInput
+                value={resumo}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setResumo(e.target.value)
+                }
+                placeholder="Do que se trata o chamado?"
+              />
+            </Fieldset>
+            <Fieldset legend="Tipo">
+              <Picker
+                value={tipo}
+                onValueChange={(value) => setTipo(value)}
+                items={[
+                  {
+                    label: "Falta de material",
+                    value: "Falta de material",
+                  },
+                  {
+                    label: "Problema com a internet",
+                    value: "Problema com a internet",
+                  },
+                  {
+                    label: "Solicitação de limpeza",
+                    value: "Solicitação de limpeza",
+                  },
+                  {
+                    label: "Solicitação de recurso",
+                    value: "Solicitação de recurso",
+                  },
+                  {
+                    label: "Objeto perdido",
+                    value: "Objeto perdido",
+                  },
+                  {
+                    label: "Outros",
+                    value: "Outros",
+                  },
+                ]}
+                style={OpenCallPicker}
+              />
+            </Fieldset>
+            <Fieldset legend="Descrição">
+              <OpenCallInput
+                height="240px"
+                placeholder="Nos conte mais detalhes sobre o ocorrido..."
+                value={descricao}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setDescricao(e.target.value)
+                }
+              />
+            </Fieldset>
+            <Fieldset legend="Data do ocorrido">
+              <OpenCallInput
+                type="date"
+                value={selectedDate}
+                onChange={handleInputChange}
+                placeholder="dd/mm/aaaa"
+                keyboardType="numeric"
+                mask="dd/mm/aaaa"
+                maxLength="10"
+                pattern="23/12/2022"
+              />
+            </Fieldset>
+          </OpenCallContent>
+          <FotterButtons>
+            <ConfirmButton onClick={() => CreateTicket()}>
+              Confirmar
+            </ConfirmButton>
+          </FotterButtons>{" "}
+        </>
+      ) : (
+        <Spinner visible={loading} size={"large"} color="red"></Spinner>
+      )}
     </OpenCallContainer>
   );
 };
